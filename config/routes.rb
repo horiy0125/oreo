70377b758cb7858ca8905cd9ef61e59db14f02f2
horiy0125 <k.horiy0125@gmail.com>
2022-07-16 17:09:53 +0900

    feat: introduce devise_token_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  namespace :admin do
+  end
 
   namespace :api do
     get '/health_check' => 'health_check#index'
+
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
   end
 
 end
